5b1e746e285f55d801b432fd7ecaefcb8c3b1b1a
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-24 11:01:05 -0300

    feat: add user verification to show correctly routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 
+import { useAuth } from '../hooks/auth/useAuth';
 import PrivateRoutes from './private.routes';
 import PublicRoutes from './public.routes';
 
-const Routes = () => (
-  <NavigationContainer>
-    <PublicRoutes />
-  </NavigationContainer>
-);
+const Routes = () => {
+  const { user } = useAuth();
+
+  return (
+    <NavigationContainer>
+      {user?.id ? <PrivateRoutes /> : <PublicRoutes />}
+    </NavigationContainer>
+  );
+};
 
 export default Routes;
