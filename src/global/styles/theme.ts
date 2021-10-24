90d6a7a648ad404bb57aeb2304fa90288ec9ccc7
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-24 11:42:40 -0300

    feat: add common colors to default theme object

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -17,34 +17,38 @@ const DEFAULT_THEME_OBJECT = {
     heading: moderateScale(12),
     body: moderateScale(15),
   },
+  colors: {
+    primary: '#772ce8',
+    red500: '#e91916',
+    white100: '#ffffff',
+    gray200: '#adadb8',
+    green200: '#00f593',
+    orange200: '#FFAC3C',
+  },
 };
 
 export const lightTheme = {
   ...DEFAULT_THEME_OBJECT,
   colors: {
+    ...DEFAULT_THEME_OBJECT?.colors,
+
     background: '#f7f7f8',
-    primary: '#772ce8',
     text500: '#0e0e10',
     text400: 'rgba(0, 0, 0, 0.7)',
     text300: '#53535f',
-
     gray100: 'rgba(0, 0, 0, 0.05)',
-    red500: '#e91916',
-    white100: '#ffffff',
   },
 };
 
 export const darkTheme = {
   ...DEFAULT_THEME_OBJECT,
   colors: {
+    ...DEFAULT_THEME_OBJECT?.colors,
+
     background: '#0e0e10',
-    primary: '#772ce8',
     text500: '#efeff1',
     text400: 'rgba(255, 255, 255, 0.7)',
     text300: '#adadb8',
-
     gray100: 'rgba(255, 255, 255, 0.15)',
-    red500: '#e91916',
-    white100: '#ffffff',
   },
 };
