d782035aafb848e30bdaa60faf8fb04069d96b16
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-24 11:40:32 -0300

    refactor: refactor code

--- src/components/SignInSocialButton/styles.ts
+++ src/components/SignInSocialButton/styles.ts
@@ -1,36 +1,44 @@
 import { FontAwesome5 } from '@expo/vector-icons';
 import { ActivityIndicator } from 'react-native';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 export const Container = styled.Pressable`
-  height: 56px;
-  background-color: ${({ theme }) => theme.colors.white100};
-  border-radius: 8px;
-  align-items: center;
-  flex-direction: row;
+  ${({ theme }) => css`
+    height: 56px;
+    background-color: ${theme.colors.white100};
+    border-radius: 8px;
+    align-items: center;
+    flex-direction: row;
+  `}
 `;
 
 export const IconContainer = styled.View`
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-  border-color: ${({ theme }) => theme.colors.background};
-  border-right-width: 1px;
-  padding: 16px;
+  ${({ theme }) => css`
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    border-color: ${theme.colors.background};
+    border-right-width: 1px;
+    padding: 16px;
+  `}
 `;
 
 export const Icon = styled(FontAwesome5)`
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: 24px;
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    font-size: 24px;
+  `}
 `;
 
 export const ButtonText = styled.Text`
-  flex: 1;
-  text-align: center;
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: ${({ theme }) => theme.fontSize.small}px;
-  font-family: ${({ theme }) => theme.fonts.semibold};
-  line-height: ${({ theme }) => theme.lineHeight.body}px;
+  ${({ theme }) => css`
+    flex: 1;
+    text-align: center;
+    color: ${theme.colors.primary};
+    font-size: ${theme.fontSize.small}px;
+    font-family: ${theme.fonts.semibold};
+    line-height: ${theme.lineHeight.body}px;
+  `}
 `;
 
 export const LoadingIndicator = styled(ActivityIndicator).attrs(
