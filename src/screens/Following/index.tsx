b2585f2276f45e2397525625729b49eba195c594
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-26 11:23:48 -0300

    feat: add TopGames and FollowedLiveStreams components to Following screen

--- src/screens/Following/index.tsx
+++ src/screens/Following/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import FollowedLiveStreams from '../../components/FollowedLiveStreams';
 import Header from '../../components/Header';
+import TopGames from '../../components/TopGames';
 import { Container } from './styles';
 
 const Following = () => {
   return (
     <Container>
       <Header />
+
+      <TopGames />
+      <FollowedLiveStreams />
     </Container>
   );
 };
