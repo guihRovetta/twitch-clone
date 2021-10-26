eee1f6a4f407e04f7bb307bd00c23a72f8290252
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-26 09:12:32 -0300

    feat: add intl package

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import {
 } from '@expo-google-fonts/inter';
 import AppLoading from 'expo-app-loading';
 import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 
 import { ThemeModeProvider } from './src/hooks/themeMode/ThemeModeContext';
 import Root from './src/screens/Root';
