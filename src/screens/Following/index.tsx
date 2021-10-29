dfec6f24ce64b5464416f84246ff8dd11868c5b6
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-28 22:43:13 -0300

    feat: add FollowingFlatList element and data for this element

--- src/screens/Following/index.tsx
+++ src/screens/Following/index.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import FollowedLiveStreams from '../../components/FollowedLiveStreams';
 import Header from '../../components/Header';
+import PageHeading from '../../components/PageHeading';
+import SectionTitle from '../../components/SectionTitle';
 import TopGames from '../../components/TopGames';
-import { Container } from './styles';
+import { Container, FollowingFlatList } from './styles';
+
+export type FollowingItem = {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+};
 
 const Following = () => {
+  const { data, indices } = useMemo(() => {
+    const items: FollowingItem[] = [
+      {
+        key: 'heading',
+        render: () => <PageHeading>Seguindo</PageHeading>,
+      },
+
+      {
+        key: 'top_games_title',
+        render: () => <SectionTitle>Top categorias</SectionTitle>,
+        isTitle: true,
+      },
+      { key: 'top_games_component', render: () => <TopGames /> },
+
+      {
+        key: 'live_channels_title',
+        render: () => <SectionTitle>Seus canais ao vivo</SectionTitle>,
+        isTitle: true,
+      },
+      { key: 'live_channels_component', render: () => <FollowedLiveStreams /> },
+    ];
+
+    const indices: number[] = [];
+
+    items?.forEach((item, index) => item.isTitle && indices?.push(index));
+
+    return {
+      data: items,
+      indices,
+    };
+  }, []);
+
   return (
     <Container>
       <Header />
 
-      <TopGames />
-      <FollowedLiveStreams />
+      <FollowingFlatList
+        data={data}
+        renderItem={({ item }) => item?.render()}
+        keyExtractor={(item) => item?.key}
+        stickyHeaderIndices={indices}
+      />
     </Container>
   );
 };
--- src/screens/Following/styles.ts
+++ src/screens/Following/styles.ts
@@ -1,7 +1,20 @@
+import { FlatList } from 'react-native';
 import styled, { css } from 'styled-components/native';
+
+import { FollowingItem } from '.';
 
 export const Container = styled.View`
   ${({ theme }) => css`
     background-color: ${theme.colors.background};
+    flex: 1;
   `}
 `;
+
+export const FollowingFlatList = styled(
+  FlatList as new () => FlatList<FollowingItem>
+).attrs({
+  contentContainerStyle: {
+    paddingHorizontal: 16,
+    paddingBottom: 16,
+  },
+})``;
