2198398fd64ef4552684f27ed4db1d4b5ac1f0ef
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-28 22:39:35 -0300

    feat: create empty mocks

---
+++ src/components/TopGames/mock.ts
@@ -0,0 +1,9 @@
+import { CategoryType } from '../CategoryCard';
+
+export const EMPTY_TOP_GAMES: CategoryType[] = [
+  { id: '1', name: 'Stream 1', url: 'no-image', countValue: '1' },
+  { id: '2', name: 'Stream 2', url: 'no-image', countValue: '2' },
+  { id: '3', name: 'Stream 3', url: 'no-image', countValue: '3' },
+  { id: '4', name: 'Stream 4', url: 'no-image', countValue: '4' },
+  { id: '5', name: 'Stream 5', url: 'no-image', countValue: '5' },
+];
