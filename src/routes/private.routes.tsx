275d8f0c4de6cfcd7a17bacdefae85c4516f6cfb
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-30 12:12:06 -0300

    feat: change bottom tab style to use BlurView from expo-blur

--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -4,15 +4,19 @@ import {
   AntDesign,
 } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BlurView } from 'expo-blur';
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { useTheme } from 'styled-components';
 
+import { useThemeMode } from '../hooks/themeMode/useThemeMode';
 import Following from '../screens/Following';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 const PrivateRoutes = () => {
   const theme = useTheme();
+  const { themeMode } = useThemeMode();
 
   return (
     <Navigator
@@ -25,9 +29,16 @@ const PrivateRoutes = () => {
           fontFamily: theme.fonts.regular,
         },
         tabBarStyle: {
-          backgroundColor: theme.colors.background,
-          borderTopWidth: 0,
+          borderTopColor: theme?.colors.gray100,
+          position: 'absolute',
         },
+        tabBarBackground: () => (
+          <BlurView
+            tint={themeMode === 'light' ? 'light' : 'dark'}
+            intensity={100}
+            style={StyleSheet.absoluteFill}
+          />
+        ),
       }}
     >
       <Screen
