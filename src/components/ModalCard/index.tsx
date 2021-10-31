e55792c4cd92916349ba950584898ae4c98985f5
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-31 15:45:04 -0300

    fix: adjust style when pass hasPadding prop

--- src/components/ModalCard/index.tsx
+++ src/components/ModalCard/index.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { Container } from './style';
 
 type ModalCardProps = {
+  hasPadding?: boolean;
   children: React.ReactNode;
 };
 
-const ModalCard = ({ children }: ModalCardProps) => {
-  return <Container>{children}</Container>;
+const ModalCard = ({ hasPadding = false, children }: ModalCardProps) => {
+  return <Container hasPadding={hasPadding}>{children}</Container>;
 };
 
 export default ModalCard;
--- src/components/ModalCard/style.ts
+++ src/components/ModalCard/style.ts
@@ -1,9 +1,18 @@
 import styled, { css } from 'styled-components/native';
 
-export const Container = styled.View`
-  ${({ theme }) => css`
+const hasPaddingStyle = css`
+  padding: 20px;
+`;
+
+type ContainerProps = {
+  hasPadding?: boolean;
+};
+
+export const Container = styled.View<ContainerProps>`
+  ${({ theme, hasPadding }) => css`
     background-color: ${theme.colors.modal.highlight};
-    border-radius: 16px;
-    padding: 16px;
+    border-radius: 12px;
+
+    ${!!hasPadding && hasPaddingStyle};
   `};
 `;
