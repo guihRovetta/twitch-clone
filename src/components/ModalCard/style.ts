38c9026ea6c3f39c9e0698470cabdc643f427f52
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-10-31 14:36:31 -0300

    fix: fix element and adjust style to match the layout

--- src/components/ModalCard/style.ts
+++ src/components/ModalCard/style.ts
@@ -1,7 +1,9 @@
 import styled, { css } from 'styled-components/native';
 
-export const Container = styled.Text`
+export const Container = styled.View`
   ${({ theme }) => css`
     background-color: ${theme.colors.modal.highlight};
+    border-radius: 16px;
+    padding: 16px;
   `};
 `;
