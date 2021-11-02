716c4859fa164f93ba504e956abbb234bd0035e3
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-11-02 10:41:36 -0300

    feat: add fullWidth, color, borderRadiusType and isLoading prop to Button component, adjust style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import { TouchableOpacityProps } from 'react-native';
 
-import { Container, ButtonWrapper, ButtonText } from './styles';
+import {
+  Container,
+  ButtonWrapper,
+  ButtonText,
+  LoadingIndicator,
+} from './styles';
+
+export type ColorType = 'primary' | 'disabled';
+export type BorderRadiusType = 'normal' | 'large';
 
 type ButtonProps = TouchableOpacityProps & {
   icon?: React.ReactNode;
+  fullWidth?: boolean;
+  color?: ColorType;
+  borderRadiusType?: BorderRadiusType;
+  isLoading?: boolean;
 };
 
-const Button = ({ icon, ...rest }: ButtonProps) => {
+const Button = ({
+  icon,
+  fullWidth = false,
+  color = 'primary',
+  borderRadiusType = 'normal',
+  isLoading = false,
+  ...rest
+}: ButtonProps) => {
   return (
     <Container {...rest}>
-      <ButtonWrapper>
+      <ButtonWrapper
+        fullWitdh={fullWidth}
+        color={color}
+        borderRadiusType={borderRadiusType}
+      >
         {icon}
-        <ButtonText>{rest?.children}</ButtonText>
+        {!isLoading ? (
+          <ButtonText color={color}>{rest?.children}</ButtonText>
+        ) : (
+          <LoadingIndicator />
+        )}
       </ButtonWrapper>
     </Container>
   );
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,23 +1,70 @@
-import styled, { css } from 'styled-components/native';
+import { ActivityIndicator } from 'react-native';
+import styled, { css, DefaultTheme } from 'styled-components/native';
+
+import { BorderRadiusType, ColorType } from '.';
 
 export const Container = styled.TouchableOpacity``;
 
-export const ButtonWrapper = styled.View`
-  ${({ theme }) => css`
-    padding: 8px 16px;
+const buttonBorderRadiusModifiers = {
+  large: () => css`
+    border-radius: 24px;
+  `,
+
+  normal: () => css`
+    border-radius: 8px;
+  `,
+};
+
+const buttonColorModifiers = {
+  primary: (theme: DefaultTheme) => css`
     background-color: ${theme.colors.primary};
+  `,
+
+  disabled: (theme: DefaultTheme) => css`
+    background-color: ${theme.colors.gray100};
+  `,
+};
+
+type ButtonWrapperProps = {
+  fullWitdh?: boolean;
+  color?: ColorType;
+  borderRadiusType?: BorderRadiusType;
+};
+
+export const ButtonWrapper = styled.View<ButtonWrapperProps>`
+  ${({ theme, fullWitdh, color, borderRadiusType }) => css`
+    padding: 10px 16px;
     border-radius: 24px;
     flex-direction: row;
     align-items: center;
-    align-self: baseline;
+    align-self: ${fullWitdh ? 'stretch' : 'baseline'};
+    justify-content: center;
+
+    ${!!color && buttonColorModifiers[color](theme)};
+    ${!!borderRadiusType && buttonBorderRadiusModifiers[borderRadiusType]()};
   `}
 `;
 
-export const ButtonText = styled.Text`
-  ${({ theme }) => css`
+const buttonTextColorModifiers = {
+  primary: (theme: DefaultTheme) => css`
     color: ${theme.colors.white100};
-    font-size: ${theme.fontSize.small}px;
+  `,
+
+  disabled: (theme: DefaultTheme) => css`
+    color: ${theme.colors.gray300};
+  `,
+};
+
+export const ButtonText = styled.Text<Pick<ButtonWrapperProps, 'color'>>`
+  ${({ theme, color }) => css`
+    font-size: ${theme.fontSize.regular}px;
     font-family: ${theme.fonts.semibold};
     line-height: ${theme.lineHeight.body}px;
+
+    ${!!color && buttonTextColorModifiers[color](theme)};
   `}
 `;
+
+export const LoadingIndicator = styled(ActivityIndicator).attrs({
+  size: 16,
+})``;
